75a83efb1e231cb856ec7afe7c8a0161c59b159c
Nicks Home Computer <vaughnick@gmail.com>
2019-01-21 22:18:07 -0500

    primitive state management

--- src/app/shows/shows.component.ts
+++ src/app/shows/shows.component.ts
@@ -1,5 +1,6 @@
 import { ExampleModel } from './../shared/example.model';
 import { Component, OnInit } from '@angular/core';
+import { UUID } from 'angular2-uuid';
 
 
 
@@ -7,20 +8,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-shows',
   template: `
   <submit-form label="Show" (value)="onShow($event)"></submit-form>
-
+  <list title="List of shows" [list]="example.store"></list>
   `
 })
 export class ShowsComponent implements OnInit {
+
 
   constructor(public example: ExampleModel) { }
 
   ngOnInit() { }
 
   onShow(text: string) {
-    console.log(text)
-    this.example.store.push(text);
+    console.log('text: ', text);
+    let uuid = UUID.UUID();
+    console.log('uuid: ', uuid);
+    let pushed: any = { title: text, subtitle: `ID:${uuid}` }
+    this.example.store.push(pushed);
     console.log(this.example.store)
-    // console.log(this.store.store)
 
   }
 
